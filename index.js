e5d5d6e52f9699288f518386d35c4acc8d302665
Joseph Parker <jparker010190@gmail.com>
2018-07-07 22:24:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file
 function findMatching(drivers, string){
-  const result = drivers.filter(num)(string => num  )
+  const result = drivers.filter(num)(string == num  )
   return result
 }
