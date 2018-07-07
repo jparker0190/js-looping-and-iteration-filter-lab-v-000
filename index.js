3ea811ab436a56649ff69a9071dd9fcb1b879456
Joseph Parker <jparker010190@gmail.com>
2018-07-07 23:02:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function findMatching(driver, string){
 }
 function fuzzyMatch(driver, string){
   var dri = driver.filter(function(num){
-    return num.toLowerCase().includes(string.toLowerCase()) === num;
+    return num.toLowerCase().charAt(string.toLowerCase());
   })
   return dri
 }
