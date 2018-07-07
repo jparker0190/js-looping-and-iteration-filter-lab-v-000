ca192df319b56f17c90a9c3084e82a72346d01fe
Joseph Parker <jparker010190@gmail.com>
2018-07-07 22:31:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file
-function findMatching(drivers, string){
-  drivers.filter(string)
+function findMatching(driver, string){
+  driver.fitler(function(num) {return num = string})
 }
