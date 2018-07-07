d406be02422383e74307404267a7f1a7667de0b5
Joseph Parker <jparker010190@gmail.com>
2018-07-07 22:55:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function findMatching(driver, string){
   var str = string.toLowerCase();
   var dri = driver.filter(function(num) {
     driver.toLowerCase
-    return num.includes(string)
+    return num.includes(str)
   })
   return dri
 }
