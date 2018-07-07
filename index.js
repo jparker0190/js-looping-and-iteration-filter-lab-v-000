9974e6b62bd1915f9b881171806b96295f7084e2
Joseph Parker <jparker010190@gmail.com>
2018-07-07 23:11:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function fuzzyMatch(driver, string){
 }
 function matchName(driver, string){
   var dri = driver.filter(function(num){
-    return driver[name] === num
+    return num.getElementsByClassName(string)
   })
   return dri
 }
