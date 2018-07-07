a68f772c0857704003ea07942aa1d77305949ade
Joseph Parker <jparker010190@gmail.com>
2018-07-07 22:50:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Code your solution in this file
 function findMatching(driver, string){
+  string.toLowerCase()
   var dri = driver.filter(function(num) {return num.includes(string)})
   return dri
 }
